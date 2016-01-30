fa7f2073d27dbe51e8ca43888c654dcd91b1d3df
Sashe <sash239@gmail.com>
2016-01-30 22:37:31 +0100

    Solution for problem 1 of Euler project in the scope of The Odin Project

--- euler-solutions/problem-1.js
+++ euler-solutions/problem-1.js
@@ -2,14 +2,9 @@
 //  Find the sum of all the multiples of 3 or 5 below 1000.
 
 function euler() {
-    var number1 = 3,
-        number2 = 5,
-        sum = 0,
-        i = 1;
+    var sum = 0, i = 1;
     while (i < 1000) {
-        if (i % 3 === 0 || i % 5 === 0) {
-            sum += i;
-        }
+        if (i % 3 === 0 || i % 5 === 0) sum += i;
         i++;
     }
     return sum;
