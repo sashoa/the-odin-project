33a2efec50e02b0c9a704bcc2563fe939e515439
Sashe <sash239@gmail.com>
2016-02-24 13:28:46 +0100

    added rails-projects

--- euler-solutions/problem-3.js
+++ euler-solutions/problem-3.js
@@ -1,23 +1,23 @@
 function euler() {
-    var num = 976,
-        factor = [],
-        seq = [],
-        isprime = false;
-    for (var  i = 1; i <= num ; i++)
-        if (num % i === 0)
-            factor.push(i);
-    if (num % 2 === 0) seq.push(2);
-    for ( i = 0; i < factor.length -1 ; i++) {
-    	for (var j = 2; j < factor[i]; j++) {
-    		if (factor[i] !== 1 && factor[i] !== j){
-    			if (factor[i] % j === 0) isprime = false; 
-    			else isprime = true;
-    		}
-    		if (isprime === false) break;
-    		else continue;
-    	}
-    	if (isprime === true) seq.push(factor[i]);
+  var num = 976,
+  factor = [],
+  seq = [],
+  isprime = false;
+  for (var  i = 1; i <= num ; i++)
+    if (num % i === 0)
+      factor.push(i);
+  if (num % 2 === 0) seq.push(2);
+  for ( i = 0; i < factor.length -1 ; i++) {
+    for (var j = 2; j < factor[i]; j++) {
+      if (factor[i] !== 1 && factor[i] !== j){
+        if (factor[i] % j === 0) isprime = false; 
+        else isprime = true;
+      }
+      if (isprime === false) break;
+      else continue;
     }
-    console.log(seq);
+    if (isprime === true) seq.push(factor[i]);
+  }
+  console.log(seq);
 }
 euler();
